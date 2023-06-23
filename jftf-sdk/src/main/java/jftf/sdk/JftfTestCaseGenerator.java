1ce1a3f1a87731f815569112fdbc0916a7cb6d71
Decebal <radulescudecebal@outlook.com>
2023-06-24 01:09:10 +0300

    SNAPSHOT-1.0/Experimental - Added env variable empty check in TestCaseGenerator

--- jftf-sdk/src/main/java/jftf/sdk/JftfTestCaseGenerator.java
+++ jftf-sdk/src/main/java/jftf/sdk/JftfTestCaseGenerator.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.Objects;
 
 public class JftfTestCaseGenerator {
     private static final String TEST_CASE_TEMPLATE_FILE = "vtemplates/JftfTestCaseTemplate.vm";
@@ -30,7 +31,7 @@ public class JftfTestCaseGenerator {
             String className = System.getenv("CLASS_NAME");
             String testGroup = System.getenv("TEST_GROUP");
 
-            if (className == null || testGroup == null) {
+            if (Objects.equals(className, "") || Objects.equals(testGroup, "")) {
                 System.out.println("Please set the CLASS_NAME and TEST_GROUP environment variables.");
                 return;
             }
